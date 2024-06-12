f1ed26231ec1b5256af943f97957e2bd56c6095e
Xurshidjon <161340754+Xurshidjondartdev@users.noreply.github.com>
2024-06-12 13:23:04 +0500

    clean

--- lib/main.dart
+++ lib/main.dart
@@ -29,7 +29,7 @@ class _HomePageState extends State<HomePage> {
   List<Widget> contenerList1 = [];
   List<Widget> contenerList2 = [];
   List<Widget> contenerList3 = [];
-  List<String> zikr = [ 'Subhan-\nAlloh','Alhamdu-\nli-lloh', 'Allohu-\nAkbar'];
+  List<String> zikr = ['Subhan-\nAlloh', 'Alhamdu-\nli-lloh', 'Allohu-\nAkbar'];
 
   void nullBoottom() {
     couter = 0;
@@ -62,168 +62,164 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body:Container(
-        decoration:const BoxDecoration(
+      body: Container(
+        decoration: const BoxDecoration(
           image: DecorationImage(
             image: AssetImage('assets/images/img2.png'),
             fit: BoxFit.cover,
           ),
         ),
-        child:SizedBox(
-        height: double.infinity,
-        width: double.infinity,
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.end,
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Expanded(
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.end,
-                      children: contenerList1,
+        child: SizedBox(
+          height: double.infinity,
+          width: double.infinity,
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Expanded(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: contenerList1,
+                      ),
                     ),
-                  ),
-                  Expanded(
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.end,
-                      children: contenerList2,
+                    Expanded(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: contenerList2,
+                      ),
                     ),
-                  ),
-                  Expanded(
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.end,
-                      children: contenerList3,
+                    Expanded(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: contenerList3,
+                      ),
                     ),
-                  ),
-                ],
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Container(
-                    padding: const EdgeInsets.all(10),
-                    height: 60,
-                    width: 90,
-                    child: Text('${zikr[zikrcounter]} '),
-                    decoration: BoxDecoration(
-                      
-                      borderRadius: BorderRadius.circular(10),
-                      border: Border.all(color: Colors.black),
-                      color: Colors.amber,
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Container(
+                      padding: const EdgeInsets.all(10),
+                      height: 60,
+                      width: 90,
+                      child: Text('${zikr[zikrcounter]} '),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        border: Border.all(color: Colors.black),
+                        color: Colors.amber,
+                      ),
                     ),
-                  ),
-                  MaterialButton(
-                    onPressed: () {
-                      increment();
-                      if (couter > 0 && couter <= 11) {
-                        contenerList1.add(
-                          Container(
-                            margin: const EdgeInsets.all(5),
-                            alignment: Alignment.center,
-                            height: 50,
-                            width: 50,
-                            child: Text(
-                              '$couter',
-                              style: const TextStyle(fontSize: 30),
-                            ),
-                            decoration: BoxDecoration(
-                              color: Colors.amber,
-
-                              shape: BoxShape.circle,
-                              border: Border.all(
-                                color: Colors.black,
-                                width: 2,
-                                strokeAlign: BorderSide.strokeAlignCenter,
-                                style: BorderStyle.solid,
+                    MaterialButton(
+                      onPressed: () {
+                        increment();
+                        if (couter > 0 && couter <= 11) {
+                          contenerList1.add(
+                            Container(
+                              margin: const EdgeInsets.all(5),
+                              alignment: Alignment.center,
+                              height: 50,
+                              width: 50,
+                              child: Text(
+                                '$couter',
+                                style: const TextStyle(fontSize: 30),
+                              ),
+                              decoration: BoxDecoration(
+                                color: Colors.amber,
+                                shape: BoxShape.circle,
+                                border: Border.all(
+                                  color: Colors.black,
+                                  width: 2,
+                                  strokeAlign: BorderSide.strokeAlignCenter,
+                                  style: BorderStyle.solid,
+                                ),
                               ),
                             ),
-                          ),
-                        );
-                      } else if (couter >= 12 && couter <= 22) {
-                        contenerList2.add(
-                          Container(
-                            margin: const EdgeInsets.all(5),
-                            alignment: Alignment.center,
-                            height: 50,
-                            width: 50,
-                            child: Text(
-                              '$couter',
-                              style: const TextStyle(fontSize: 30),
-                            ),
-                            decoration: BoxDecoration(
-                              color: Colors.amber,
-
-                              shape: BoxShape.circle,
-                              border: Border.all(
-                                color: Colors.black,
-                                width: 2,
-                                strokeAlign: BorderSide.strokeAlignCenter,
-                                style: BorderStyle.solid,
+                          );
+                        } else if (couter >= 12 && couter <= 22) {
+                          contenerList2.add(
+                            Container(
+                              margin: const EdgeInsets.all(5),
+                              alignment: Alignment.center,
+                              height: 50,
+                              width: 50,
+                              child: Text(
+                                '$couter',
+                                style: const TextStyle(fontSize: 30),
+                              ),
+                              decoration: BoxDecoration(
+                                color: Colors.amber,
+                                shape: BoxShape.circle,
+                                border: Border.all(
+                                  color: Colors.black,
+                                  width: 2,
+                                  strokeAlign: BorderSide.strokeAlignCenter,
+                                  style: BorderStyle.solid,
+                                ),
                               ),
                             ),
-                          ),
-                        );
-                      } else if (couter >= 23 && couter <= 33) {
-                        contenerList3.add(
-                          Container(
-                            margin: const EdgeInsets.all(5),
-                            alignment: Alignment.center,
-                            height: 50,
-                            width: 50,
-                            child: Text(
-                              '$couter',
-                              style: const TextStyle(fontSize: 30),
-                            ),
-                            decoration: BoxDecoration(
-                              color: Colors.amber,
-                              shape: BoxShape.circle,
-                              border: Border.all(
-                                color: Colors.black,
-                                width: 2,
-                                strokeAlign: BorderSide.strokeAlignCenter,
-                                style: BorderStyle.solid,
+                          );
+                        } else if (couter >= 23 && couter <= 33) {
+                          contenerList3.add(
+                            Container(
+                              margin: const EdgeInsets.all(5),
+                              alignment: Alignment.center,
+                              height: 50,
+                              width: 50,
+                              child: Text(
+                                '$couter',
+                                style: const TextStyle(fontSize: 30),
+                              ),
+                              decoration: BoxDecoration(
+                                color: Colors.amber,
+                                shape: BoxShape.circle,
+                                border: Border.all(
+                                  color: Colors.black,
+                                  width: 2,
+                                  strokeAlign: BorderSide.strokeAlignCenter,
+                                  style: BorderStyle.solid,
+                                ),
                               ),
                             ),
-                          ),
-                        );
-                      }
-                    },
-                    child: Text(
-                      couterAsosiy.toString(),
-                      style: const TextStyle(fontSize: 52, shadows: [
-                        Shadow(
-                          blurRadius: 50,
-                        )
-                      ]),
+                          );
+                        }
+                      },
+                      child: Text(
+                        couterAsosiy.toString(),
+                        style: const TextStyle(fontSize: 52, shadows: [
+                          Shadow(
+                            blurRadius: 50,
+                          )
+                        ]),
+                      ),
+                      color: Colors.blueAccent,
+                      height: 110,
+                      minWidth: 200,
+                      shape: const CircleBorder(
+                          side: BorderSide(width: 2, color: Colors.black)),
+                      elevation: 20,
                     ),
-                    color: Colors.blueAccent,
-                    height: 110,
-                    minWidth: 200,
-                    shape: const CircleBorder(
-                        side: BorderSide(width: 2, color: Colors.black)),
-                    elevation: 20,
-                  ),
-                  MaterialButton(
-                    onPressed: () {
-                      nullBoottom();
-                    },
-                    color: Colors.redAccent,
-                    shape: const StadiumBorder(),
-                    child: const Icon(
-                      Icons.restart_alt,
+                    MaterialButton(
+                      onPressed: () {
+                        nullBoottom();
+                      },
+                      color: Colors.redAccent,
+                      shape: const StadiumBorder(),
+                      child: const Icon(
+                        Icons.restart_alt,
+                      ),
+                      elevation: 20,
                     ),
-                    elevation: 20,
-                  ),
-                ],
-              ),
-            ],
+                  ],
+                ),
+              ],
+            ),
           ),
         ),
       ),
-      ),
-      
     );
   }
 }
